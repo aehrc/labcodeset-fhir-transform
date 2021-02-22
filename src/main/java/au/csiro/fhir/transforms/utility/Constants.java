cee1f0612127ab3dbab14d9b6e7a2f150d0f670f
dionmcm <dion.mcmurtrie@gmail.com>
2021-02-23 09:05:01 +1000

    Version 1.0.1 with updated copyright and addition of missing runtime logging library to remove a start up warning message

--- src/main/java/au/csiro/fhir/transforms/utility/Constants.java
+++ src/main/java/au/csiro/fhir/transforms/utility/Constants.java
@@ -52,7 +52,15 @@ public class Constants {
   /**
    * Copyright statement set on all Labcodeset generated resources
    */
-  public static final String LABCODESET_RESOURCE_COPYRIGHT = "";
+  public static final String LABCODESET_RESOURCE_COPYRIGHT =
+      "The Labcodeset is developed and maintained by NVKC (Nederlandse Vereniging voor Klinische Chemie en Laboratoriumgeneeskunde) and NVMM (Nederlandse Vereniging voor Medische Microbiologie), with functional maintenance by Nictiz (Nationaal ICT Instituut in de Zorg). If you have any questions or change requests, please email labcodeset@nictiz.nl.\n"
+          + "The Labcodeset is based on LOINC, UCUM and SNOMED. Its use requires a license for the Dutch SNOMED Edition, which can be obtained at https://mlds.ihtsdotools.org/.\n"
+          + "This material contains content from LOINC (http://loinc.org). LOINC is copyright © 1995-2020, Regenstrief Institute, Inc. and the Logical Observation Identifiers Names and Codes (LOINC) Committee and is available at no cost under the license at http://loinc.org/license. LOINC® is a registered United States trademark of Regenstrief Institute, Inc.\n"
+          + "In general, Regenstrief’s terms-of-use for LOINC and UCUM have had the same core principles since its first release back in 1995. Certainly, the license has gotten longer and more explicit about certain things, but the main premises remain:\n"
+          + "1. LOINC and UCUM are free for use in both commercial and non-commercial applications,\n"
+          + "2. LOINC and UCUM are protected against change by end-users or creation of another vocabulary so that they can fulfil their goal of being an international standard, and\n"
+          + "3. When LOINC or UCUM are used in databases, software applications, etc. users need to acknowledge the license and copyright so that end users of that product know that LOINC and UCUM are protected and licensed in a specific way.\n"
+          + "The license agreements for LOINC and UCUM are available respectively at https://loinc.org/license/ and https://unitsofmeasure.org/trac/wiki/TermsOfUse";
 
   /**
    * Prefix used for URIs for Labcodeset generated resources
